c7cd0c06bbb8efa8e3373284aa8628736c035bef
lakxnaut <lakshyaa.nautiyal@gmail.com>
2026-01-16 12:56:25 +0530

    hhj

--- models/Course.js
+++ models/Course.js
@@ -4,6 +4,7 @@ const courseSchema = new Schema(
   {
     organizationId: { type: Types.ObjectId, ref: 'Organization', required: true },
     title: { type: String, required: true, trim: true },
+    imageUrl: { type: String, default: '' },
     description: { type: String, default: '' },
     basePrice: { type: Number, required: true, min: 0 },
     createdBy: { type: Types.ObjectId, ref: 'User', required: true },
