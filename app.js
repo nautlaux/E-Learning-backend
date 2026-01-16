08b1204ada84cbb382f67565a8eea5529135bc96
lakxnaut <lakshyaa.nautiyal@gmail.com>
2026-01-16 12:03:05 +0530

    fixed cors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
+// Allow CORS for frontend clients (adjust origin as needed)
+app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
